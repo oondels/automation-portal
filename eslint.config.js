3a928327977bce9f277319b606412453370b3e17
Hendrius Dass <hendriusfelix.dev@gmail.com>
2025-06-13 16:56:06 -0300

    chore: Update .gitignore, package.json, and package-lock.json; add new dependencies and adjust environment variable handling

--- eslint.config.js
+++ eslint.config.js
@@ -18,11 +18,16 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
     },
     rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
+    
   }
 );
+
+
