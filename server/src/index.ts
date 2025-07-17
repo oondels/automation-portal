edf8f49e0bda721275cf905da62fd00778693682
Hendrius Dass <hendriusfelix.dev@gmail.com>
2025-07-17 15:17:11 -0300

    feat: configure server port using environment variables for improved flexibility

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,10 +3,11 @@ import cors from "cors"
 import http from "http"
 import { projectRoute } from "./routes/project.route"
 import { WsManager } from './websockets/manager';
+import { config } from "./config/env" // Load and validate environment variables
 
 (async () => {
   const app = express();
-  const PORT = 9137;
+  const PORT = config.port;
 
   app.use(cors())
   app.use(express.json())
@@ -15,11 +16,13 @@ import { WsManager } from './websockets/manager';
   const server = http.createServer(app)
   const wsManager = new WsManager(server)
 
+  // Health check
   app.get("/", (_, res: Response) => {
     res.status(200).json({ message: "Automation Service is running!" });
   })
 
   server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://${config.env === 'development' ? 'localhost' : '10.100.1.43'}:${PORT}`);
   })
 })()
+
