5b42175fd9d5a759ffae10e6b0f7a4689818c067
Hendrius Dass <hendriusfelix.dev@gmail.com>
2025-07-18 10:12:46 -0300

    refactor: restructure server initialization for improved readability and error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,25 +4,51 @@ import http from "http"
 import { projectRoute } from "./routes/project.route"
 import { WsManager } from './websockets/manager';
 import { config } from "./config/env" // Load and validate environment variables
+import { AppError } from './utils/AppError';
+import { AppDataSource } from './config/data-source';
 
-(async () => {
-  const app = express();
-  const PORT = config.port;
+AppDataSource.initialize()
+  .then(() => {
 
-  app.use(cors())
-  app.use(express.json())
-  app.use("/api/projects/", projectRoute)
+    (async () => {
+      const app = express();
+      const PORT = config.port;
 
-  const server = http.createServer(app)
-  const wsManager = new WsManager(server)
+      app.use(cors())
+      app.use(express.json())
+      app.use("/api/projects/", projectRoute)
+      // Error Handler
+      app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+        console.error(`Error on method ${req.method} - ${req.originalUrl}:`, error);
+        if (error instanceof AppError) {
+          res.status(error.statusCode).json({ message: error.message });
+          return
+        }
 
-  // Health check
-  app.get("/", (_, res: Response) => {
-    res.status(200).json({ message: "Automation Service is running!" });
+        res.status(500).json({
+          message: "Internal Server Error",
+          error: config.env === 'development' ? error.message || "An unexpected error occurred" : undefined
+        });
+        return
+      })
+
+      const server = http.createServer(app)
+      const wsManager = new WsManager(server)
+
+      // Health check
+      app.get("/", (_, res: Response) => {
+        res.status(200).json({ message: "Automation Service is running!" });
+      })
+
+      server.listen(PORT, () => {
+        console.log(`Server is running on http://${config.env === 'development' ? 'localhost' : '10.100.1.43'}:${PORT}`);
+      })
+    })();
+
   })
+  .catch(error => {
+    console.error("Error initializing DataSource", error);
+    process.exit(1);
+  });
 
-  server.listen(PORT, () => {
-    console.log(`Server is running on http://${config.env === 'development' ? 'localhost' : '10.100.1.43'}:${PORT}`);
-  })
-})()
 
