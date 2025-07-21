aab9b04734c6579932dffd5501a438f0321a5d03
Hendrius Dass <hendriusfelix.dev@gmail.com>
2025-07-21 15:49:37 -0300

    feat: implement permission checking middleware to manage user access based on roles and positions

---
+++ server/src/middlewares/checkPermission.middleware.ts
@@ -0,0 +1,59 @@
+import { Request, Response, NextFunction } from "express";
+
+// TODO: Fazer configuração geral -> Marcenaria, Serralheria e Automação
+// TODO: Tranferir armazenamento de usuarios liberados para tabela propria
+type RolePermissionMap = {
+  [action: string]: {
+    allowedRoles: string[],
+    allowedUsers?: string[],
+    allowedPositions?: string[],
+  };
+};
+
+const permissionMap: RolePermissionMap = {
+  approveProject: {
+    allowedRoles: ["GERENTE"],
+    allowedUsers: ["SERGIO.GONCALVES", "EDUARDO.BISOL", "HENDRIUS.SANTANA"]
+  },
+  updateEstimatedTime: {
+    allowedRoles: ["ANALISTA", "COORDENADOR"],
+    allowedPositions: ["AUTOMACAO", "TI", "MARCENEIRO", "SERRALHEIRO"]
+  },
+  attendProject: {
+    allowedRoles: ["ANALISTA", "AUTOMACAO"],
+    allowedPositions: ["AUTOMACAO", "TI", "MARCENEIRO", "SERRALHEIRO"]
+  },
+};
+
+// Verifica o ususario, funcao e permissões
+// Se o usuario não tiver permissão, retorna 403
+// Se o usuario tiver permissão, chama o next
+export const CheckPermission = (action: keyof typeof permissionMap, role: string = "") => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user;
+    const permission = permissionMap[action];
+
+    if (!user || !permission) {
+      res.status(403).json({ message: "Acesso negado. Permissão não definida." });
+      return
+    }
+    if (!permission.allowedRoles || permission.allowedRoles.length === 0) {
+      res.status(403).json({ message: "Acesso negado. Nenhuma função permitida." });
+      return
+    }
+
+    const roleAllowed = permission.allowedRoles.includes(user.funcao.toUpperCase());
+    const userAllowed = permission.allowedUsers ? permission.allowedUsers.includes(user?.usuario as string) : true;
+    const positionAllowed = permission.allowedPositions ? permission.allowedPositions.includes(user?.funcao as string) : true;
+
+    if ((roleAllowed && positionAllowed) || (roleAllowed && userAllowed) || user.funcao.toUpperCase() === role.toUpperCase()) {
+      next();
+      return
+    }
+
+    res.status(403).json({
+      message: "Acesso negado! Você não possui acesso a essa funcionalidade. Entre em contato com o suporte."
+    });
+    return
+  };
+};
