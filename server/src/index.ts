74bdae7804c9922041558bdb9ee136dbc7a36afc
Hendrius Dass <hendriusfelix.dev@gmail.com>
2025-07-21 13:45:24 -0300

    feat: add cookie-parser dependency and update type definitions

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from "cors"
 import http from "http"
+import cookieParser from "cookie-parser"
 import { projectRoute } from "./routes/project.route"
 import { WsManager } from './websockets/manager';
 import { config } from "./config/env" // Load and validate environment variables
@@ -15,6 +16,7 @@ AppDataSource.initialize()
       const PORT = config.port;
 
       app.use(cors())
+      app.use(cookieParser())
       app.use(express.json())
       app.use("/api/projects/", projectRoute)
       // Error Handler
