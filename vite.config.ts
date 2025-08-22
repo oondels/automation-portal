6eacef3c71922875c02f8eb9d968f17684e79b6b
Hendrius Dass <hendriusfelix.dev@gmail.com>
2025-08-22 08:42:38 -0300

    feat: update base path in Vite configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/automacao-dass/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
