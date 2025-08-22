0e39afec76fcf949c28f9d6566034e2129808dd2
Hendrius Dass <hendriusfelix.dev@gmail.com>
2025-08-22 13:18:52 -0300

    refactor: remove service worker registration logic from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
-if (window.isSecureContext && 'serviceWorker' in navigator) {
-  const { registerSW } = await import('virtual:pwa-register');
-  registerSW({ immediate: true });
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
