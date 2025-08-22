def1a27f4a67d4abee32d6358bf34040a82cca75
Hendrius Dass <hendriusfelix.dev@gmail.com>
2025-08-22 13:06:07 -0300

    feat: enable service worker registration in main.tsx and add sourcemap support in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
-  // base: mode === 'development' ? '/' : '/automacao-dass/',
+  base: mode === 'development' ? '/' : '/automacao-dass/',
   plugins: [react()],
   optimizeDeps: {},
+  build: { sourcemap: true },
 }));
